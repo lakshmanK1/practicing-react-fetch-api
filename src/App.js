84832b144fbc504f6c50cf22d1e17982a7f02f31
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-11-11 21:48:12 +0530

    Handeling the errors while fetching data

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import React,{useState} from "react";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState()
 
   async function handleFetch (){ 
-    setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
+    try{
+      setIsLoading(true);
+    setError(null);
+    const response = await fetch('https://swapi.dev/api/film/');
+    
+    if(!response.ok){
+      throw new Error("Something went wrong!");
+    }
+
     const data = await response.json();
  
     const transformedMovies = data.results.map((item)=>{
@@ -19,7 +27,14 @@ function App() {
       }
     })
     setMovies(transformedMovies);
+    
+    }catch(error){
+      setError(error.message)
+    }
     setIsLoading(false);
+
+    
+
   }  
   
   
@@ -27,8 +42,10 @@ function App() {
     <div>
      <center><button onClick={handleFetch}>Show Movies</button></center>
       {!isloading && movies.length>0 && <MovieList movies={movies}/>}
-      {!isloading && movies.length === 0 && <center><p>No Movies Found...</p></center>}
+      {!isloading && movies.length === 0 && !error && <center><p>No Movies Found...</p></center>}
+      {!isloading && error && <center><p>Something went wrong..</p></center>}
       {isloading && <center><p>loading....</p></center>}
+      
     </div>
   );
 }
