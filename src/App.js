451179dd114b41bf4aad6ae39c7ea56118820419
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-11-11 19:18:09 +0530

    created the fetch api request to fetch the json data to browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieList from "./components/MoviesList";
+import React,{useState} from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const handleFetch = () => { 
+    fetch('https://swapi.dev/api/films/').then((response)=>{
+    return response.json();
+  }).then((data)=>{
+    const transformedMovies = data.results.map((item)=>{
+      return {
+        id:item.episode_id,
+        title:item.title,
+        openingText:item.opening_crawl,
+        releaseDate:item.release_date
+      }
+    })
+    setMovies(transformedMovies);
+  })  
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <center><button onClick={handleFetch}>Show Movies</button></center>
+      <MovieList movies={movies}/>
     </div>
   );
 }
